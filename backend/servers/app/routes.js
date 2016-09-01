54e4a3f2f2241e22260abf0a171216342a1cb494
Martin Chuckeles <martin@jakubik.info>
2016-09-01 23:08:09 +0200

    Fix the build path and a couple of other issues

--- backend/servers/app/routes.js
+++ backend/servers/app/routes.js
@@ -11,7 +11,7 @@ const Router = express.Router;
  * App router.
  */
 module.exports = Router()
-    .use('/static', express.static(`${process.cwd()}/frontend/static`))
+    .use('/static', express.static(`${process.cwd()}/frontend/build`))
     .get('*', (req, res) => {
         res.sendFile(`${process.cwd()}/frontend/index.html`);
     });
