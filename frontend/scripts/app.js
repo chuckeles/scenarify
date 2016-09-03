6d9d03146c623fbe4f08bac9341d9d9b5ce4e9c4
Martin Chuckeles <martin@jakubik.info>
2016-09-03 17:43:40 +0200

    Set up vue-loader and Webpack
    This should be the complete setup of Webpack with watching,
    hot-reloading, loaders, etc. Closes #23.

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,1 +1,18 @@
-console.log('Scenarify is starting!');
+/**
+ * The entry script for the Scenarify app.
+ * Loads and sets up Vue.
+ */
+
+
+import Vue from 'vue';
+
+
+/**
+ * Create Vue instance.
+ */
+new Vue({
+    el: 'body',
+    components: {
+
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,63 @@
  */
 
 
+const path = require('path');
+
+
 module.exports = {
 
     entry: './frontend/scripts/app.js',
     output: {
         path: './frontend/build',
+        publicPath: '/static',
         filename: 'scripts.js'
+    },
+
+    devtool: 'source-map',
+    devServer: {
+        historyApiFallback: true,
+        progress: true,
+        hot: true,
+        inline: true,
+        contentBase: path.join(process.cwd(), 'frontend'),
+        proxy: {
+            '*': {
+                target: 'http://localhost:3000',
+                secure: false
+            }
+        }
+    },
+
+    resolveLoader: {
+        root: path.join(process.cwd(), 'node_modules'),
+    },
+    module: {
+        loaders: [
+
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['transform-runtime']
+                }
+            },
+
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file',
+                query: {
+                    name: '[name].[ext]'
+                }
+            }
+
+        ]
     }
 
 };
